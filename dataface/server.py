9e69745e92a66d52c8dcee15db9beddc47c50340
jjh68069 <jahi3695@colorado.edu>
2017-11-07 09:59:18 -0700

    pushing server

--- dataface/server.py
+++ dataface/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 import flask_login
+import time
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -8,9 +9,16 @@ Bootstrap(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == "POST":
-		with open('data', 'w') as f:
-			f.write(str(request.form))
-	return render_template('index.html')
+		with open('data', 'a') as f:
+			f.write("--- POST FROM " + str(request.headers.get('User-Agent')) + " ---" + time.strftime("%H:%M:%S") + "\n")
+			f.write(str(request.form) + "\n")
+			for key in request.form:
+				f.write("  " + str(key) + " - " + str(request.form[key]) + "\n")
+                        print(str(request.headers))
+                        print(str(request.form) + "\n\n")
+		return "<html><body>Success!</body></html>"
+	else:
+		return render_template('index.html')
 
 @app.route('/map')
 def map():
