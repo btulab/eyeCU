263d08a6d36a1a44261407f438b6ec75f8eb314a
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2017-11-24 15:12:42 -0700

    new look for client

---
+++ client/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+import flask_login
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == "POST":
+		with open('data', 'w') as f:
+			f.write(str(request.form))
+	return render_template('index.html')
+
+@app.route('/elements')
+def elements():
+    return render_template('elements.html')
+
+@app.route('/map')
+def map():
+	return render_template('map.html')
+
+@app.route('/login')
+def login():
+	return 0
+
