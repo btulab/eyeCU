1b743dbf8b15acf76da2b28672c6dbef053e220a
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2017-12-23 22:49:26 -0700

    small improvements

--- client/auth.py
+++ client/auth.py
@@ -1,7 +1,7 @@
 from passlib.hash import pbkdf2_sha256
 import configparser
 import MySQLdb
-import string, random
+import string, random, sys
 
 dbcreds = configparser.ConfigParser()
 dbcreds.read('db.cfg')
@@ -10,40 +10,50 @@ db = MySQLdb.connect(host=dbcreds.get('database', 'host'),
                      passwd=dbcreds.get('database', 'passwd'),
                      db=dbcreds.get('database', 'db'))
 
-# credentials
-username = "admin"
-password = "morty"
+def createUser(username, password):
 
-# add a random salt
-salt = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(40))
-print(salt)
-password = password + salt
-print(password)
+    # check if user already exists
+    cur = db.cursor()
+    cur.execute("SELECT COUNT(1) FROM Users WHERE email = %s;", [username])
+    if cur.fetchone()[0]:
+        print("%s already exists in database." % username)
+        exit()
 
-passhash = pbkdf2_sha256.encrypt(password, rounds=200000, salt_size=16)
-print(passhash)
+    # add a random salt
+    salt = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(40))
+    print(salt)
+    password = password + salt
+    print(password)
 
-# insert into database
-query = "INSERT INTO Users(email,salt,hash) " \
-        "VALUES(%s,%s,%s)"
-args = (username,salt,passhash)
-cur = db.cursor()
-cur.execute(query,args)
-db.commit()
+    passhash = pbkdf2_sha256.encrypt(password, rounds=200000, salt_size=16)
+    print(passhash)
 
-# check database entry to verify
-# grab salt
-cur.execute("SELECT salt FROM Users WHERE email = %s;", [username])
-salt = cur.fetchall()
-print("salt")
-print(salt)
-print(salt[0][0])
+    # insert into database
+    query = "INSERT INTO Users(email,salt,hash) " \
+             "VALUES(%s,%s,%s)"
+    args = (username,salt,passhash)
+    cur.execute(query,args)
+    db.commit()
 
-# hash
-cur.execute("SELECT hash FROM Users WHERE email = %s;", [username])
-passhash = cur.fetchall()
-print("excess")
-print(passhash[0][0])
+    # check database entry to verify
+    # grab salt
+    cur.execute("SELECT salt FROM Users WHERE email = %s;", [username])
+    salt = cur.fetchall()
+    print("salt")
+    print(salt)
+    print(salt[0][0])
 
-print(pbkdf2_sha256.verify(password, passhash[0][0]))
+    # hash
+    cur.execute("SELECT hash FROM Users WHERE email = %s;", [username])
+    passhash = cur.fetchall()
+    print("excess")
+    print(passhash[0][0])
+    print(pbkdf2_sha256.verify(password, passhash[0][0]))
 
+if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        print("Usage: python auth.py [username] [password]")
+        print(sys.argv)
+        quit()
+    createUser(sys.argv[1], sys.argv[2])
