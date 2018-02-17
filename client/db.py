a7ad095aa2f3653fee1d966ba54f2980dc903fe1
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2018-02-17 16:23:38 -0700

    Fix for database issues

--- client/db.py
+++ client/db.py
@@ -5,10 +5,11 @@ dbcreds = configparser.ConfigParser()
 dbcreds.read('db.cfg')
 
 def connection():
+
     db = MySQLdb.connect(host=dbcreds.get('database', 'host'),
                      user=dbcreds.get('database', 'user'),
                      passwd=dbcreds.get('database', 'passwd'),
                      db=dbcreds.get('database', 'db'))
-
+    
     cur = db.cursor()
-    return db, cur 
+    return db,cur
