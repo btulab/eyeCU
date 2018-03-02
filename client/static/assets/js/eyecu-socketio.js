a3781388ffc940324de79d1f3c3125c7edb6e436
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2018-03-01 22:18:26 -0700

    fix typo and typing speed

--- client/static/assets/js/eyecu-socketio.js
+++ client/static/assets/js/eyecu-socketio.js
@@ -2,7 +2,7 @@ var socket;
 $(document).ready(function() {
   var socket = io.connect('http://' + document.domain + ':' + location.port);
   socket.on('connect', function() {
-    $('#statusBar').prepend("<p class='anim=typewriter' id=status>Listening for network updates...</p>");
+    $('#statusBar').prepend("<p class='anim-typewriter' id='status'>Listening for network updates...</p>");
   });
   socket.on('update', function(data) {
     $('#statusBar').prepend("<p class='anim-typewriter' id='status'>"+ data.msg +"</p>");
