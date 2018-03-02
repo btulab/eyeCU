4ddc3241cb9e30b8707eba74f11826685527c9e4
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2018-03-01 22:08:25 -0700

    typewriter effect for statusbar

--- client/static/assets/js/eyecu-socketio.js
+++ client/static/assets/js/eyecu-socketio.js
@@ -2,10 +2,10 @@ var socket;
 $(document).ready(function() {
   var socket = io.connect('http://' + document.domain + ':' + location.port);
   socket.on('connect', function() {
-    $('#statusBar').prepend("<p id=status>Listening for network updates...</p>");
+    $('#statusBar').prepend("<p class='anim=typewriter' id=status>Listening for network updates...</p>");
   });
   socket.on('update', function(data) {
-    $('#statusBar').prepend("<p id=status>"+ data.msg +"</p>");
+    $('#statusBar').prepend("<p class='anim-typewriter' id='status'>"+ data.msg +"</p>");
   });
 });
 
