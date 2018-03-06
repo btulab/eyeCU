69a716794c341a8b0ad9d891b373d7e884490773
Carlos <carlos.lawrence@colorado.edu>
2018-03-06 16:12:24 -0700

    Added device statistics

--- client/app.py
+++ client/app.py
@@ -236,7 +236,7 @@ def device(device_to_display):
 	if(device_name == "error"):
 		flash("Invalid device ID")
 		return redirect('/map')
-	cur.execute("SELECT * FROM Data WHERE deviceID=" + device_to_display + " ORDER BY timeRecieved desc LIMIT 1000")
+	cur.execute("SELECT * FROM Data WHERE deviceID=" + device_to_display + " ORDER BY timeRecieved desc LIMIT 2016")
 	for row in cur.fetchall():
 		data_timeRecieved.append(strftime("%d %b - %H:%M", localtime(int(row[1]))))
 		data_light.append(row[2])
@@ -251,6 +251,7 @@ def device(device_to_display):
 		data_sound.append(row[11])
 	if (len(data_timeRecieved) == len(data_light) == len(data_motion) == len(data_pressure) == len(data_temperature) == len(data_humidity) == len(data_co2) == len(data_button) == len(data_altitude) == len(data_voc) == len(data_sound)):
 		data = {"timeRecieved": list(reversed(data_timeRecieved)), "light": list(reversed(data_light)), "motion": list(reversed(data_motion)), "pressure": list(reversed(data_pressure)), "temperature": list(reversed(data_temperature)), "humidity": list(reversed(data_humidity)), "co2": list(reversed(data_co2)), "button": list(reversed(data_button)), "altitude": list(reversed(data_altitude)), "voc": list(reversed(data_voc)), "sound": list(reversed(data_sound))} #all data has to be reversed to re-order it chronologically
+		data['numSubmissions'] = len(data_timeRecieved)
 		cur.close()
 		return render_template('display_one_device.html', device=device_name, data=data)
 	else:
